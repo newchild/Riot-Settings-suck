97aef9043ac40ce729adcae099f9d9f41c22b255
newchild <fabianbrand98@gmx.de>
2015-02-01 21:01:29 +0100

    Huehue

---
+++ Riot Settings/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Riot_Settings
+{
+	public partial class Form1 : Form
+	{
+		public static string Filepath = "";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.DefaultExt = ".cfg";
+			openFileDialog1.ShowDialog();
+			if (openFileDialog1.CheckFileExists)
+			{
+				Filepath = openFileDialog1.FileName;
+				Settings_Editor editor = new Settings_Editor();
+				editor.Show();
+				//this.Close();
+			}
+			else
+			{
+				this.Close();
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button1.Text = "Select game.cfg...";
+		}
+	}
+}
---
+++ Riot Settings/Settings Editor.cs
@@ -0,0 +1,68 @@
+﻿using System;	
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using IniParser;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IniParser.Model;
+
+namespace Riot_Settings
+{
+	public partial class Settings_Editor : Form
+	{
+		Dictionary<string, string[]> help = new Dictionary<string, string[]>();
+		Dictionary<string, string> help2 = new Dictionary<string, string>();
+		FileIniDataParser parser = new FileIniDataParser();
+		IniData data;
+		public Settings_Editor()
+		{
+			InitializeComponent();
+		}
+
+		private void Settings_Editor_Load(object sender, EventArgs e)
+		{
+			data = parser.ReadFile(Form1.Filepath);
+			foreach (var Option in data.Sections)
+			{
+				comboBox1.Items.Add(Option.SectionName);
+				
+			}
+		
+			
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			textBox1.Text = data[comboBox1.SelectedItem.ToString()][comboBox2.SelectedItem.ToString()];
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			comboBox2.Items.Clear();
+			foreach (var Key in data.Sections)
+			{
+				if (Key.SectionName == comboBox1.SelectedItem.ToString())
+				{
+					foreach (var ActualKey in Key.Keys)
+					{
+						comboBox2.Items.Add(ActualKey.KeyName);
+					}
+				}
+			}
+
+			
+			
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			data[comboBox1.SelectedItem.ToString()][comboBox2.SelectedItem.ToString()] = textBox1.Text;
+			parser.WriteFile(Form1.Filepath, data);
+		}
+	}
+}
